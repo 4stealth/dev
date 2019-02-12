b6a9b31d0d7b517e1d853fadbb9c710838462b3c
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-02-13 00:02:47 +0700

    Update debo.sh

--- debo.sh
+++ debo.sh
@@ -5,3 +5,6 @@ local=/home/afrizal/project/root/
 variant=minbase
 code_name=stable
 keyring=ED05F7B2EC3C9224
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED05F7B2EC3C9224
+sudo debootstrap --arch=amd64 --variant=minbase stable /home/afrizal/project/root/ http://kartolo.sby.datautama.net.id/parrot

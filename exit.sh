aedf20ee4137a0a67f1c34364c1cf8f7a69754e4
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-02-13 22:36:38 +0700

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# Baris Perintah di dalam chroot
 apt-get clean && apt-get autoremove && rm -rf /tmp/* ~/.bash_history
 umount /proc && umount /sys && umount /dev/pts
 exit
+# Baris Perintah setelah keluar chroot
 sudo umount root/dev

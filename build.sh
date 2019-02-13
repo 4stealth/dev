6bd778d563b54b2998fcdc7b058965fb836996b9
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-02-13 11:03:54 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt-get update
 apt-cache search linux-image
-apt-get install --no-install-recommends linux-image-686 live-boot systemd-sysv
+apt-get install --no-install-recommends linux-image-amd64 live-boot systemd-sysv

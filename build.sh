c42f279983b5a95922eedf87332df1fdff85ccf5
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-02-13 11:18:52 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 apt-get update
 apt-cache search linux-image
 apt-get install --no-install-recommends linux-image-amd64 live-boot systemd-sysv
+apt-get install --no-install-recommends network-manager net-tools wireless-tools wpagui curl openssh-client blackbox xserver-xorg-core xserver-xorg xinit xterm nano 

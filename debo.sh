84efc4f60954dc05ff82feabe7af8fbcda5260b7
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-02-13 22:38:45 +0700

    Update debo.sh

--- debo.sh
+++ debo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Infomasi Distro
 server=http://kartolo.sby.datautama.net.id/parrot
 arch=amd64
 local=/home/afrizal/project/root/
@@ -6,5 +7,6 @@ variant=minbase
 code_name=stable
 keyring=ED05F7B2EC3C9224
 
+# Memasang Key dan Memulai Debootstrap
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED05F7B2EC3C9224
 sudo debootstrap --arch=amd64 --variant=minbase stable /home/afrizal/project/root/ http://kartolo.sby.datautama.net.id/parrot
